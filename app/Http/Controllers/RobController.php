0ed0c08636612b646cd2a62ad2f23dbe8dbe042f
Gene Piki <pikigene01@gmail.com>
2022-09-11 12:52:42 +0200

    gene backend

--- app/Http/Controllers/RobController.php
+++ app/Http/Controllers/RobController.php
@@ -9,11 +9,4 @@ class RobController extends Controller
    public function index(Request $request){
        dd('rob');
    }
-
-   public function show($id)
-   {
-       return view('user.profile', [
-           'user' => User::findOrFail($id)
-       ]);
-   }
 }
